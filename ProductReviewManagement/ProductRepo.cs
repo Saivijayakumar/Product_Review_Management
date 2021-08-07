0324e2161627e24935d49ed455c89f5a426f6324
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-08 00:20:00 +0530

    [SaiVijayaKumar]Createing List Of Products

---
+++ ProductReviewManagement/ProductRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class ProductRepo
+    {
+        //Adding Product to list
+        public int AddProductReviewToList(List<ProductReview> products)
+        {
+            try
+            {
+                products.Add(new ProductReview() { productId = 1, userId = 5, review = "Good", rating = 9, isLike = true });
+                products.Add(new ProductReview() { productId = 3, userId = 9, review = "Bad", rating = 6, isLike = false });
+                products.Add(new ProductReview() { productId = 5, userId = 4, review = "Average", rating = 5, isLike = true });
+                products.Add(new ProductReview() { productId = 1, userId = 10, review = "Average", rating = 10, isLike = true });
+                products.Add(new ProductReview() { productId = 4, userId = 7, review = "Good", rating = 5, isLike = true });
+                products.Add(new ProductReview() { productId = 1, userId = 1, review = "Good", rating = 8, isLike = true });
+                products.Add(new ProductReview() { productId = 2, userId = 2, review = "Average", rating = 8, isLike = true });
+                products.Add(new ProductReview() { productId = 1, userId = 2, review = "Bad", rating = 8, isLike = false });
+                products.Add(new ProductReview() { productId = 5, userId = 9, review = "Average", rating = 7, isLike = true });
+                products.Add(new ProductReview() { productId = 3, userId = 11, review = "Good", rating = 5, isLike = true });
+                products.Add(new ProductReview() { productId = 3, userId = 4, review = "Good", rating = 9, isLike = true });
+                products.Add(new ProductReview() { productId = 3, userId = 9, review = "Bad", rating = 6, isLike = false });
+                products.Add(new ProductReview() { productId = 5, userId = 4, review = "Average", rating = 3, isLike = true });
+                products.Add(new ProductReview() { productId = 2, userId = 5, review = "Bad", rating = 7, isLike = false });
+                products.Add(new ProductReview() { productId = 2, userId = 1, review = "Very Good", rating = 9, isLike = true });
+                products.Add(new ProductReview() { productId = 7, userId = 7, review = "Very Good", rating = 8, isLike = true });
+                products.Add(new ProductReview() { productId = 9, userId = 5, review = "Very Good", rating = 7, isLike = true });
+                products.Add(new ProductReview() { productId = 10, userId = 3, review = "Bad", rating = 9, isLike = false });
+                products.Add(new ProductReview() { productId = 12, userId = 3, review = "Bad", rating = 6, isLike = false });
+                products.Add(new ProductReview() { productId = 14, userId = 15, review = "Very Good", rating = 9, isLike = true });
+
+                IterateList(products);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return products.Count;
+        }
+        public static void IterateList(List<ProductReview> products)
+        {
+            try
+            {
+                foreach (ProductReview i in products)
+                {
+                    Console.WriteLine($"ProductId:{i.productId}\t UserId:{i.userId}\t Review:{i.review}\tRating:{i.rating}\tIsLike:{i.isLike}\t");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+
+    }
+}
---
+++ UnitTestForProductReviewManagement/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductReviewManagement;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestForProductReviewManagement
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ProductRepo  repo;
+        List<ProductReview> Products;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            Products = new List<ProductReview>();
+            repo = new ProductRepo();
+        }
+        [TestMethod]
+        public void AddingDataToListAndReturnTheCount()
+        {
+            int actual, expected = 20;
+            actual = repo.AddProductReviewToList(Products);
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
